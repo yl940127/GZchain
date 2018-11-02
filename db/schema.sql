245722fa6684945c35e6d4a45bbbc331c0f8bc67
yl940127 <1173867960@qq.com>
2018-11-02 12:37:43 +0800

    修复数据库表bug

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,16 @@
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `product`;
 DROP TABLE IF EXISTS `share`;
 DROP TABLE IF EXISTS `user_share`;
 
 CREATE TABLE  `users`(
-    userId      INT(20)     NOT NULL AUTO_INCREMENT
+    userId      INT(20)             NOT NULL AUTO_INCREMENT
     COMMENT 'id',
-    username    VARCHAR(20) NOT NULL
+    username    VARCHAR(20)         NOT NULL
     COMMENT '用户名',
-    PASSWORD    VARCHAR(20) NOT NULL
+    PASSWORD    VARCHAR(20)         NOT NULL
     COMMENT '密码',
-    email       VARCHAR(20) NOT NULL
+    email       VARCHAR(20)         NOT NULL
     COMMENT '邮箱',
     realName    VARCHAR(20)
     COMMENT '真实姓名',
@@ -20,9 +20,9 @@ CREATE TABLE  `users`(
     COMMENT '身份证',
     birthday     DATETIME
     COMMENT '生日',
-    education   VARCHAR(19),
+    education   VARCHAR(20)
     COMMENT '教育',
-    sex         INT
+    gender        CHAR(8)
     COMMENT '性别',
     headImgUrl  VARCHAR(200)
     COMMENT '头像',
@@ -31,29 +31,30 @@ CREATE TABLE  `users`(
   COMMENT '用户表';
 
 CREATE TABLE `product` (
-  id          INT(20)        NOT NULL AUTO_INCREMENT
+  id          INT(20)               NOT NULL  AUTO_INCREMENT
   COMMENT 'id',
-  name        VARCHAR(32)    NOT NULL
+  productName        VARCHAR(32)    NOT NULL
   COMMENT '商品名称',
-  price       DECIMAL(10, 2) NOT NULL
+  price       DECIMAL(10, 2)        NOT NULL
   COMMENT '价格',
-  status      CHAR(20)       NOT NULL
+  STATUS      CHAR(20)              NOT NULL
   COMMENT '商品状态',
-  store_num   INT(20)                 DEFAULT 0
+  store_num   INT(20)               DEFAULT 0
   COMMENT '库存',
-  sale_num    INT(20)                 DEFAULT 0
+  sale_num    INT(20)               DEFAULT 0
   COMMENT '销量',
-  image_urls  VARCHAR(512)   NOT NULL
+  image_urls  VARCHAR(512)          NOT NULL
   COMMENT '商品图片url',
-  comment_num INT(20)                 DEFAULT 0
+  comment_num INT(20)               DEFAULT 0
+  COMMENT '评论数',
   PRIMARY KEY `id`(`id`)
 )
   COMMENT '商品表';
 
 CREATE TABLE `share`(
-  id          INT(20)       NOT NULL AUTO_INCREMENT
+  id          INT(20)              NOT NULL AUTO_INCREMENT
   COMMENT 'id',
-  product_id  INT(20)       NOT NULL
+  product_id  INT(20)              NOT NULL
   COMMENT '商品id',
   PRIMARY KEY `id`(`id`),
   KEY `product_id`(`product_id`)
@@ -61,11 +62,11 @@ CREATE TABLE `share`(
   COMMENT '商品分享表';
 
 CREATE TABLE `user_share`(
-  id         INT(20)  NOT NULL AUTO_INCREMENT
+  id         INT(20)               NOT NULL AUTO_INCREMENT
   COMMENT 'id',
-  user_id    INT(20)  NOT NULL
+  user_id    INT(20)               NOT NULL
   COMMENT '用户id',
-  share_id   INT(20)  NOT NULL
+  share_id   INT(20)               NOT NULL
   COMMENT '商品分享id',
   PRIMARY KEY `id`(`id`),
   KEY `user_id`(`user_id`),
